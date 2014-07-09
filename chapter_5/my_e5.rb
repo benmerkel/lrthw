2015d70b6bffb2dc44ad14a9a70a6c1272c4b107
Ben Merkel <ben@merkelfamily.com>
2014-07-08 23:55:19 -0600

    renamed ex5-2.rb

--- chapter_5/my_e5.rb
+++ chapter_5/my_e5.rb
@@ -11,14 +11,17 @@ hair = 'Brown'
 weight_kilos = weight / 2.2
 height_cm  = height * 2.54
 
-
+# adds a formatted string of the variable <name>
 puts "Let's talk about %s." % name
+# adds two formatted decimals of the variables <height> and <height_cm>
 puts "He's %d inches tall (or %d centimeters)." % [height, height_cm]
+# adds two formatted decimals of the variables <weight> and <weight_kilos>
 puts "He's %d pounds heavy (or %d kilos)." % [weight, weight_kilos]
 puts "Actually that's not too heavy."
+# adds two formatted strings for the variables <eyes> and <hair>
 puts "He's got %s eyes and %s hair" % [eyes, hair]
+# adds a formatted string of the variable <name>
 puts "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
-puts "If I add %d, %d, and %d I get %d." % [
-  age, height, weight, age + height + weight]
+puts "If I add %d, %d, and %d I get %d." % [age, height, weight, age + height + weight]
