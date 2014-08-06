8cd6ef57adfc03daabfeae84e9d7d0388142708d
Ben Merkel <ben@merkelfamily.com>
2014-08-05 22:38:05 -0600

    Fixed rubocop offenses for chapter_3

--- chapter_3/ex3.rb
+++ chapter_3/ex3.rb
@@ -1,26 +1,28 @@
 # Several strings within don't use any interpolation so only
 # single quotes were necessary.
 
-
 # Outputs a string
 puts 'I will now count my chickens:'
 
 # This is dividing 30 by 6 and then adding 25
 puts 'Hens', 25 + 30 / 6
 
-# This is multiplying 25 by 3, then remainder of 75/4 and subtracting that from 100
+# This is multiplying 25 by 3, then remainder of 75/4
+# and subtracting that from 100
 puts 'Roosters', 100 - 25 * 3 % 4
 
 # Outputs a string
 puts 'now I will count the eggs:'
 
-# Gets remainder of 4/2 (which is zero) and integer of 1/4 (also zero) and the add/subtracts the rest of the numbers
+# Gets remainder of 4/2 (which is zero) and integer of
+# 1/4 (also zero) and the add/subtracts the rest of the numbers
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 # Outputs a string
 puts 'Is it true that 3 + 2 < 5 - 7?'
 
-# checks to see if 3+2 is less than 5-7. 5 is not less than -2 so it returns false.
+# checks to see if 3+2 is less than 5-7.
+# 5 is not less than -2 so it returns false.
 puts 3 + 2 < 5 - 7
 
 # Outputs a string and the sum of 3 + 2 on the next line
