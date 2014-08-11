619769a979d232649f6fc36311e3dbbeda9e0182
Ben Merkel <ben@merkelfamily.com>
2014-08-10 21:31:26 -0600

    Removed .idea from git, added a Readme.md file, added comments to ex10.rb

---
+++ chapter_10/Readme.md
@@ -0,0 +1,10 @@
+Extra Credit questions
+
+1)  List two or three escape sequences in Readme.md, along with what they do
+\t inserts a tab
+\n new line
+\s space
+\" singe quote
+\\ backslash
+
+2)  Add this as a final line in ex10.rb
--- chapter_10/ex10.rb
+++ chapter_10/ex10.rb
@@ -1,3 +1,4 @@
+# Set variables
 tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
@@ -9,9 +10,15 @@ I'll do a list:
 \t* Catnip\n\t* Grass
 MY_HEREDOC
 
+# This will output a cat face image when run in Rubymine.
 cat = "\n\t\tA\t\tA\n\t#{'-' * 9}\n\s-\s\t() ()\s\s-\n-\t\t\t\
 o\s\t\t-\s\sMeow\n\s-\t\s/\s\\\s\t\s-\n\t\s#{'-' * 7}"
 
+# \t inserts a tab
+# \n new line
+# \s space
+
+# output various strings
 puts tabby_cat
 puts persian_cat
 puts backslash_cat
